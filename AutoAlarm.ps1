daed164c60e703c803b1ebeaef12ee740bf5a46d
raymondcjyi <110221264+raymondcjyi@users.noreply.github.com>
2024-04-15 13:05:37 +0800

    Update AutoAlarm.ps1

--- AutoAlarm.ps1
+++ AutoAlarm.ps1
@@ -10,20 +10,19 @@ $Alarm = $true
 while ($true) {
     
     while ($Alarm) {
-  	    $beepJob = Start-Job -ScriptBlock ${function:ContinuousBeep}
+  	$beepJob = Start-Job -ScriptBlock ${function:ContinuousBeep}
         $intervalSeconds = Read-Host "Enter new time interval in seconds (or 'exit' to stop)"
         if ($null -eq $intervalSeconds) {
         	continue
     	}elseif ($intervalSeconds -eq "exit") {
-		    Stop-Job -Job $beepJob
+		Stop-Job -Job $beepJob
     		Write-Host "Alarm stopped and exiting program"
-		    exit
-        
+		exit
     	}else {
        		Stop-Job -Job $beepJob
- 		    Write-Host "Alarm stopped and triggering in the next $intervalSeconds seconds"
+ 		Write-Host "Alarm stopped and triggering in the next $intervalSeconds seconds"
         	Start-Sleep -Seconds $intervalSeconds
-		    continue
-	    }
+		continue
+	}
     }
 }
