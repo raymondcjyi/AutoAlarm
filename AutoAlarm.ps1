e0a3ad9d25c8567ef3f1149db4bd6d3ca85c758e
raymondcjyi <110221264+raymondcjyi@users.noreply.github.com>
2024-04-15 13:04:29 +0800

    Update AutoAlarm.ps1
    added "exit" input to exit program

--- AutoAlarm.ps1
+++ AutoAlarm.ps1
@@ -1,5 +1,3 @@
-#AutoAlarm powershell program 
-
 function ContinuousBeep {
     while ($true) {
         [console]::Beep()
@@ -8,21 +6,24 @@ function ContinuousBeep {
 }
 
 $Alarm = $true
-# Main loop
+
 while ($true) {
     
-    while ($Alarm)
-    {
-      	$beepJob = Start-Job -ScriptBlock ${function:ContinuousBeep}
+    while ($Alarm) {
+  	    $beepJob = Start-Job -ScriptBlock ${function:ContinuousBeep}
         $intervalSeconds = Read-Host "Enter new time interval in seconds (or 'exit' to stop)"
         if ($null -eq $intervalSeconds) {
         	continue
-      	}else
-      	{
-      		  Write-Host "Alarm stopped."
-         		Stop-Job -Job $beepJob
-          	Start-Sleep -Seconds $intervalSeconds
-  		      continue
-      	}
+    	}elseif ($intervalSeconds -eq "exit") {
+		    Stop-Job -Job $beepJob
+    		Write-Host "Alarm stopped and exiting program"
+		    exit
+        
+    	}else {
+       		Stop-Job -Job $beepJob
+ 		    Write-Host "Alarm stopped and triggering in the next $intervalSeconds seconds"
+        	Start-Sleep -Seconds $intervalSeconds
+		    continue
+	    }
     }
 }
